f59f072e7af5af9c1aaa7a55ade666f0b059631e
MalinW <maalinwallander@gmail.com>
2023-01-18 23:02:21 +0100

    removing header

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,10 +2,14 @@ import About from "../home/about/About";
 import BlogSection from "../home/blogs/BlogSection";
 import Destinations from "../home/destinations/Destinations";
 import Hotels from "../home/destinations/Hotels";
+import Header from "../home/header/Header";
+import { useState } from "react";
 
 export default function Home() {
+  const [searchState, setSearchState] = useState("");
   return (
     <>
+      <Header searchState={searchState} setSearchState={setSearchState} />
       <Destinations />
       <Hotels />
       <BlogSection />
--- src/components/pages/Layout.js
+++ src/components/pages/Layout.js
@@ -1,16 +1,11 @@
-import { Outlet } from "react-router"
-import Footer from "../home/footer/Footer"
-import Header from "../home/header/Header"
-import { useState } from "react";
-
+import { Outlet } from "react-router";
+import Footer from "../home/footer/Footer";
 
 export default function Layout() {
-    const [searchState, setSearchState] = useState("");
-
-
-    return (<>
-        <Header searchState={searchState} setSearchState={setSearchState} />
-        <Outlet />
-        <Footer />
-    </>)
+  return (
+    <>
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
